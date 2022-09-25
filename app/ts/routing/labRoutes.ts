f6310a78c67e435e69618758b49ca936582164fb
Alex Tushinski <a.tushinski@vk.team>
2022-09-25 14:42:25 +0300

    font-face fixed; MCS description added

--- app/ts/routing/labRoutes.ts
+++ app/ts/routing/labRoutes.ts
@@ -2,6 +2,6 @@ export const labRoutes = {
     mcs: {
         path: "monte-carlo-simulation",
         title: "Monte Carlo Simulation",
-        description: "Lorem ipsum dolore ipsum dolore ipsum dolore ipsum dolore ipsum dolore ipsum dolore."
+        description: "Monte Carlo Simulation, also known as the Monte Carlo Method or a multiple probability simulation, is a mathematical technique, which is used to estimate the possible outcomes of an uncertain event."
     },
 }
